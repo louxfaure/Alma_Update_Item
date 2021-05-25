6966108636d08352655df3360b0dd8386ec8d898
louxfaure <louxfaure@gmail.com>
2021-05-25 15:21:55 +0200

    Ajout d'une phase de test sur les noms des champs à modifier

--- update_item.py
+++ update_item.py
@@ -6,6 +6,9 @@ import re
 import logging
 import csv
 import json
+import sys
+import requests
+import xml.etree.ElementTree as ET
 from chardet import detect
 
 #Modules maison
@@ -22,11 +25,15 @@ LOGS_DIR = os.getenv('LOGS_PATH')
 
 REGION = 'EU'
 INSTITUTION = 'ub'
-API_KEY = os.getenv('PROD_UB_BIB_API')
+API_KEY = os.getenv('TEST_UB_API')
 
 FILE_NAME = 'test'
 IN_FILE = '/media/sf_Partage_LouxBox/{}.csv'.format(FILE_NAME)
 OUT_FILE = '/media/sf_Partage_LouxBox/{}_Rapport.csv'.format(FILE_NAME)
+ITEM_XSD = 'https://developers.exlibrisgroup.com/wp-content/uploads/alma/xsd/rest_item.xsd'
+
+xsd = {'xs': 'http://www.w3.org/2001/XMLSchema'}
+
 # ERROR_FILE = '/media/sf_Partage_LouxBox/{}_Erreurs.csv'.format(FILE_NAME)
 
 # get file encoding type
@@ -34,6 +41,28 @@ def get_encoding_type(file):
     with open(file, 'rb') as f:
         rawdata = f.read()
     return detect(rawdata)['encoding']
+
+# test headers : les noms des colonnes doivent correspondre à des noms de champs d'exemplaires dans Alma
+def test_headers(headers) :
+    r = requests.get(ITEM_XSD)
+    try:
+        r.raise_for_status()  
+    except requests.exceptions.HTTPError:
+        raise HTTPError(r,self.service)
+    reponse = r.content.decode('utf-8')
+    reponsexml = ET.fromstring(reponse)
+    item_data = reponsexml.find("xs:complexType[@name='item_data']/xs:all",xsd)
+    # print(ET.tostring(item_data))
+    for field in headers:
+        if item_data.find("xs:element[@name='{}']".format(field),xsd):
+            if (item_data.find("xs:element[@name='{}']/xs:annotation/xs:appinfo/xs:tags".format(field),xsd).text == 'api get post put') :
+                log_module.info("Le champ {} peut bien être modifié par API".format(field))
+            else :
+                return (0, "Erreur nommage colonne : {} n'est pas un chanps autorisé à l'écriture dans Alma".format(field))
+            # return reponsexml.find("sru:numberOfRecords",ns).text
+        else : 
+            return (0, "Erreur nommage colonne : {} n'est pas un chanps exemplaire connu dans Alma".format(field))
+    return(1,"Test du nomage des champs terminé")
 
 #Init logger
 logs.init_logs(LOGS_DIR,SERVICE,LOGS_LEVEL)
@@ -54,30 +83,35 @@ with open(IN_FILE, 'r', encoding=from_codec, newline='') as f:
     headers = next(reader)
     print(headers)
     del headers[0]
+    code_erreur, message = test_headers(headers)
+    if code_erreur == 0 :
+        log_module.error(message)
+        sys.exit()
+    log_module.info(message)
     # We read the file
-    for row in reader:
-        if len(row) < 2:
-            continue
-        barcode = row[0]
-        # print(barcode)
-        status,item = alma_api.get_item_with_barcode(barcode, accept='json')
-        if status == "Error" :
-            report.write("{}\t{}\t{}\n").format(barcode,status,item)
-            continue
-        i = 1
-        bib_id = item["bib_data"]["mms_id"]
-        holding_id = item["holding_data"]["holding_id"]
-        item_id = item["item_data"]["pid"]
-        for field in headers:
-            item["item_data"][field] = row[i]
-            i += 1
-        status,reponse = alma_api.set_item(bib_id, holding_id, item_id, json.dumps(item), content_type='json', accept='json')
-        if status == "Error" :
-            report.write("{}\t{}\t{}\n").format(barcode,status,reponse)
-        else :
-            report.write("{}\tSuccès\tItem mis à jour\n".format(barcode,status,reponse))
+    # for row in reader:
+    #     if len(row) < 2:
+    #         continue
+    #     barcode = row[0]
+    #     # print(barcode)
+    #     status,item = alma_api.get_item_with_barcode(barcode, accept='json')
+    #     if status == "Error" :
+    #         report.write("{}\t{}\t{}\n".format(barcode,status,item))
+    #         continue
+    #     i = 1
+    #     bib_id = item["bib_data"]["mms_id"]
+    #     holding_id = item["holding_data"]["holding_id"]
+    #     item_id = item["item_data"]["pid"]
+    #     for field in headers:
+    #         item["item_data"][field] = row[i]
+    #         i += 1
+    #     status,reponse = alma_api.set_item(bib_id, holding_id, item_id, json.dumps(item), content_type='json', accept='json')
+    #     if status == "Error" :
+    #         report.write("{}\t{}\t{}\n").format(barcode,status,reponse)
+    #     else :
+    #         report.write("{}\tSuccès\tItem mis à jour\n".format(barcode,status,reponse))
         
-        log_module.info(barcode)
+    #     log_module.info(barcode)
 report.close
 
 log_module.info("FIN DU TRAITEMENT")
